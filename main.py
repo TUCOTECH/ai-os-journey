57d3a0276a0510c7cd5537bc26480bcf14830fe6
Muhammad Zage <muhammadzage180@gmail.com>
2026-06-03 02:41:28 +0100

    Week 1- Variables, Data types, String methods

---
+++ main.py
@@ -0,0 +1,31 @@
+# # # ALLAHU AKBAR THIS IS MY FIRST CODE 
+# # print('hello world!')
+# # print('WHAT IS YOUR NAME ?')
+# # MY_NAME=input('>')
+# # print('hello nice to meet you ' + MY_NAME)
+# # print('The lenght of your name is ')
+# # print(len(MY_NAME))
+# # print('WHAT IS YOUR AGE ?')
+# # AGE=input('>')
+# # print('YOU WILL BE  '+  str(int(AGE) + 1) ' NEXT YEAR')
+# print(type(45 == 0000045.0000))
+# print(type('i am a boy'))
+# print(type(23.0))
+# print(type(10000))
+# print(type(True))
+# print(type(len('MY NAME')))
+MY_NAME= "MUHAMMAD ZAGE"
+MY_AGE= 17
+MY_CITY="KANO"
+MY_PROJECT='AI OPERATING SYSTEM'
+MY_COMPANY='ZAGE TECHNOLOGIES'
+print('NAME:', MY_NAME)
+print('MY AGE:', MY_AGE)
+print('MY CITY:', MY_CITY)
+print('MY COMPANY:', MY_COMPANY)
+print('TYPE OF NAME:',type(MY_NAME))
+print('TYPE OF  AGE:', type(MY_AGE))
+print(MY_NAME.lower())
+print(MY_NAME.upper())
+print(len(MY_NAME))
+print(f"{MY_NAME} is building an AI OPERATING SYSTEM")
